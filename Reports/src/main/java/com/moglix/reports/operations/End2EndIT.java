98da48f9bfcd5c76ec1ece3a4010eed2e41c28ba
maniroy1 <manishroy.ind@gmail.com>
2017-12-05 12:06:13 +0530

    working state commit

---
+++ Reports/src/main/java/com/moglix/reports/operations/End2EndIT.java
@@ -0,0 +1,12 @@
+package com.moglix.reports.operations;
+
+import org.hyperledger.fabric.sdk.HFClient;
+
+public class End2EndIT {
+	
+	HFClient f = HFClient.createNewInstance();
+	
+	
+	//private static final TestConfig testConfig = TestConfig.getConfig();
+
+}
